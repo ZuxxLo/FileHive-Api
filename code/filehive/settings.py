40c93400d21937e2f0319bb3a9779be6c427b2b6
Nassim <102092863+ZuxxLo@users.noreply.github.com>
2024-09-14 16:36:02 +0100

    Update settings.py

--- code/filehive/settings.py
+++ code/filehive/settings.py
@@ -30,7 +30,7 @@ django.utils.encoding.force_text = force_str
 SECRET_KEY = "django-insecure-+v1krc1yd6r7vux+_pce*l14aiglyc-y*gmrvo&famp9_o!49^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #  to be updated for simulation
 ALLOWED_HOSTS = ['*']
 
